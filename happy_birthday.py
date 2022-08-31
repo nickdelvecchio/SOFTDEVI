a3c1b0a457dd5d748094897590d9d473371226cb
Nicholas Delvecchio <nmd2440@g.rit.edu>
2022-08-31 14:52:39 -0400

    happy birthday!!

---
+++ happy_birthday.py
@@ -0,0 +1,11 @@
+def bday_message():
+    name = input("Enter your name: ")
+    month = input("Enter your birth month: ")
+    day = input("Enter your birth day: ")
+    year = input("Enter your birth year: ")
+    print(name, ", your birthday is on", month, day, "," , year,"!")
+
+    bday_message()
+    bday_message()
+    bday_message()
+    
