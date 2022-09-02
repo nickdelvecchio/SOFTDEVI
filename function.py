14b55e44c3f27089e42aa227a4bbb1a0f516c68b
Nicholas Delvecchio <nmd2440@g.rit.edu>
2022-09-02 10:21:21 -0400

    Math

---
+++ function.py
@@ -0,0 +1,17 @@
+Months_in_a_year = 12
+
+def functions():
+
+current_year= input('Current year: ')
+print(current_year)
+current_month= input('current month: ')
+print(current_month)
+Birth_year= input('Birth year: ')
+print(Birth_year)
+Birth_month= input('Birth month: ')
+print (Birth_month)
+
+def main()
+    funtions()
+    functions()
+    functions()
